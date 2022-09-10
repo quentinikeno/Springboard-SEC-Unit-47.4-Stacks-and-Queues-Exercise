370f3e66a3412b35e2ffe847c728278cfc261434
quentinikeno <ikenoq@gmail.com>
2022-09-09 17:26:48 -1000

    Add enqueue method.

--- queue.js
+++ queue.js
@@ -1,46 +1,44 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+	/** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+	enqueue(val) {
+		const node = new Node(val);
+		if (this.first === null) this.first = node;
+		if (this.last !== null) this.last.next = node;
+		this.last = node;
+		this.size++;
+	}
 
-  }
+	/** dequeue(): remove the node from the start of the queue
+	 * and return its value. Should throw an error if the queue is empty. */
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+	dequeue() {}
 
-  dequeue() {
+	/** peek(): return the value of the first node in the queue. */
 
-  }
+	peek() {}
 
-  /** peek(): return the value of the first node in the queue. */
+	/** isEmpty(): return true if the queue is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {}
 }
 
 module.exports = Queue;
