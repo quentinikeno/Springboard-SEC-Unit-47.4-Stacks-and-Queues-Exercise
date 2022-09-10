8764bc58b579157cee925528dad6ddb49c988593
quentinikeno <ikenoq@gmail.com>
2022-09-09 17:57:25 -1000

    Add push  method.

--- stack.js
+++ stack.js
@@ -1,46 +1,44 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		const node = new Node(val);
+		if (this.first !== null) node.next = this.first;
+		if (this.last === null) this.last = node;
+		this.first = node;
+		this.size++;
+	}
 
-  }
+	/** pop(): remove the node from the top of the stack
+	 * and return its value. Should throw an error if the stack is empty. */
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+	pop() {}
 
-  pop() {
+	/** peek(): return the value of the first node in the stack. */
 
-  }
+	peek() {}
 
-  /** peek(): return the value of the first node in the stack. */
+	/** isEmpty(): return true if the stack is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {}
 }
 
 module.exports = Stack;
